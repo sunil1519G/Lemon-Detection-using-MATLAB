78fb61193972f13174a8b00fa055ce77be8826c7
sunil1519G <sunil12aps@gmail.com>
2018-09-26 14:57:00 +0530

    Update main.m

--- main.m
+++ main.m
@@ -2,6 +2,6 @@ clc;clear;close all;
 vid=VideoReader('fall.mp4'); 
 while hasFrame(vid)
      img = (readFrame(vid));
-     saveas(imshow(img),sprintf('shrd%d.jpg',j));
+     saveas(imshow(img),sprintf('image%d.jpg',j));
      j = j + 1;
 end
